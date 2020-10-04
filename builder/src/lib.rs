82895ba6f9a932ea92048458ccd88f1c7991feea
Mathieu Tricoire <mathieu.tricoire@gmail.com>
2020-10-04 00:03:34 -0400

    step 7

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,13 +1,15 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use proc_macro2::Span;
 use quote::quote;
 use syn::{
-    parse_macro_input, punctuated::Punctuated, Data, DataStruct, DeriveInput, Fields, FieldsNamed,
-    GenericArgument, Ident, PathArguments, PathSegment, Token, Type,
+    parse_macro_input, punctuated::Punctuated, Attribute, Data, DataStruct, DeriveInput, Fields,
+    FieldsNamed, GenericArgument, Ident, Lit, Meta, NestedMeta, PathArguments, PathSegment, Token,
+    Type,
 };
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
@@ -48,19 +50,41 @@ pub fn derive(input: TokenStream) -> TokenStream {
         if is_option(&field.ty) || is_vector(&field.ty) {
             quote! { #name: self.#name.clone() }
         } else {
-            quote! { #name: self.#name.clone().ok_or(" is not set")? }
+            let error_message = format!("{} is not set", name.clone().unwrap().to_string());
+            quote! { #name: self.#name.clone().ok_or(#error_message)? }
         }
     });
 
-    let builder_methods = fields.iter().map(|field| {
+    let builder_methods = fields.iter().flat_map(|field| {
         let name = &field.ident;
         let ty = &field.ty;
+        let mut methods = vec![];
+
         if is_vector(&field.ty) {
-            quote! {
-                fn #name(&mut self, value: #ty) -> &mut Self {
-                    self.#name = value;
-                    self
-                }
+            let mut create_original = true;
+            let inner_type = get_inner_type(ty).unwrap();
+            let name_string = name.clone().unwrap().to_string();
+            get_each_values(&field.attrs)
+                .iter()
+                .for_each(|(each_name, each_span)| {
+                    if each_name.clone() == name_string {
+                        create_original = false;
+                    }
+                    let each_name_ident = Ident::new(each_name, each_span.clone());
+                    methods.push(quote! {
+                        fn #each_name_ident(&mut self, value: #inner_type) -> &mut Self {
+                            self.#name.push(value);
+                            self
+                        }
+                    });
+                });
+            if create_original {
+                methods.push(quote! {
+                    fn #name(&mut self, value: #ty) -> &mut Self {
+                        self.#name = value;
+                        self
+                    }
+                });
             }
         } else {
             let value_type = if is_option(&field.ty) {
@@ -68,13 +92,14 @@ pub fn derive(input: TokenStream) -> TokenStream {
             } else {
                 &field.ty
             };
-            quote! {
+            methods.push(quote! {
                 fn #name(&mut self, value: #value_type) -> &mut Self {
                     self.#name = std::option::Option::Some(value);
                     self
                 }
-            }
+            });
         }
+        methods
     });
 
     let expanded = quote! {
@@ -165,4 +190,44 @@ fn get_inner_type(ty: &Type) -> Option<&Type> {
         }
     }
     None
+}
+
+fn get_each_values(attrs: &Vec<Attribute>) -> Vec<(String, Span)> {
+    attrs
+        .iter()
+        .filter_map(|attr| {
+            let is_builder = match attr.path.get_ident() {
+                Some(ident) => ident.to_string() == "builder",
+                _ => false,
+            };
+            if is_builder {
+                let meta = attr.parse_meta();
+                if let Ok(Meta::List(meta_list)) = meta {
+                    return Some(
+                        meta_list
+                            .nested
+                            .iter()
+                            .filter_map(|nested_value| {
+                                if let NestedMeta::Meta(Meta::NameValue(meta_name_value)) =
+                                    nested_value.clone()
+                                {
+                                    if meta_name_value.path.get_ident().is_some()
+                                        && meta_name_value.path.get_ident().unwrap().to_string()
+                                            == "each"
+                                    {
+                                        if let Lit::Str(lit_str) = meta_name_value.lit {
+                                            return Some((lit_str.value(), lit_str.span()));
+                                        }
+                                    }
+                                }
+                                None
+                            })
+                            .collect::<Vec<(String, Span)>>(),
+                    );
+                }
+            }
+            None
+        })
+        .flatten()
+        .collect::<Vec<(String, Span)>>()
 }
